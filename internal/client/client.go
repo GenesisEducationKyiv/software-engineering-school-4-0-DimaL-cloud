adb927e337722eff89aba215daaa0e9bf4cc0d53
Dima L <78265212+DimaL-cloud@users.noreply.github.com>
2024-06-28 19:05:52 +0300

    feat(hw4): add several more exchange rate providers
    - add PrivatBank, Fawazahmed rate apis using Chain of Responsibility pattern
    - apis response logging
    - decrease delay between retries

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,15 +1,21 @@
 package client
 
-type ExchangeRate interface {
-	GetCurrentExchangeRate() (ExchangeRateResponse, error)
-}
+import (
+	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-DimaL-cloud/internal/client/rate"
+	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-DimaL-cloud/internal/configs"
+	"net/http"
+)
 
 type Client struct {
-	ExchangeRate
+	NbuRate        rate.Rate
+	PrivatBankRate rate.Rate
+	FawazahmedRate rate.Rate
 }
 
-func NewClient() *Client {
+func NewClient(client *http.Client, configs *configs.Config) *Client {
 	return &Client{
-		ExchangeRate: NewExchangeRateClient(),
+		NbuRate:        rate.NewNbuRateClient(client, configs.Rate.APIUrls.Nbu),
+		PrivatBankRate: rate.NewPrivatBankRateClient(client, configs.Rate.APIUrls.PrivatBank),
+		FawazahmedRate: rate.NewFawazahmedRateClient(client, configs.Rate.APIUrls.Fawazahmed),
 	}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-DimaL-cloud/internal/client/rate"
 	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-DimaL-cloud/internal/configs"
 	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-DimaL-cloud/internal/models"
 	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-DimaL-cloud/internal/repository"
@@ -16,14 +17,20 @@ type Mail interface {
 	SendEmails(subject string, body string, to []string) error
 }
 
+type Rate interface {
+	GetRate() (float64, error)
+}
+
 type Service struct {
 	Subscription
 	Mail
+	Rate
 }
 
-func NewService(repositories *repository.Repository, config *configs.Config) *Service {
+func NewService(repositories *repository.Repository, config *configs.Config, initialRateClient rate.Rate) *Service {
 	return &Service{
 		Subscription: NewSubscriptionService(repositories.Subscription),
 		Mail:         NewMailService(config.Mail),
+		Rate:         NewRateService(initialRateClient),
 	}
 }
